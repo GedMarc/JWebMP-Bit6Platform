62c8c7ce8fc73e11eea9a190d0692d4126874eaf
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:53 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.sixbitplatform.SixBitPageConfigurator;
+import com.jwebmp.plugins.sixbitplatform.implementations.Bit6ExclusionsModule;
 
 module com.jwebmp.plugins.bit6 {
 	exports com.jwebmp.plugins.sixbitplatform;
@@ -9,8 +12,12 @@ module com.jwebmp.plugins.bit6 {
 	requires com.jwebmp.logmaster;
 	requires java.logging;
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with SixBitPageConfigurator;
+
+	provides IGuiceScanModuleExclusions with Bit6ExclusionsModule;
+	provides IGuiceScanJarExclusions with Bit6ExclusionsModule;
 
 	opens com.jwebmp.plugins.sixbitplatform to com.fasterxml.jackson.databind, com.jwebmp.core;
 	//opens com.jwebmp.plugins.sixbitplatform.events to com.fasterxml.jackson.databind,com.jwebmp.core;
---
+++ src/main/java/com/jwebmp/plugins/sixbitplatform/implementations/Bit6ExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.sixbitplatform.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Bit6ExclusionsModule
+		implements IGuiceScanModuleExclusions<Bit6ExclusionsModule>,
+				           IGuiceScanJarExclusions<Bit6ExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-bit6-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.bit6");
+		return strings;
+	}
+}
