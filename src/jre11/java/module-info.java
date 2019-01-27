0d6c53807c969886cbfec98f44145b65e3ace4ed
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:54 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.sixbitplatform.SixBitPageConfigurator;
-import com.jwebmp.plugins.sixbitplatform.implementations.Bit6ExclusionsModule;
-
 module com.jwebmp.plugins.bit6 {
 	exports com.jwebmp.plugins.sixbitplatform;
 	exports com.jwebmp.plugins.sixbitplatform.features;
@@ -14,10 +8,10 @@ module com.jwebmp.plugins.bit6 {
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with SixBitPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.sixbitplatform.SixBitPageConfigurator;
 
-	provides IGuiceScanModuleExclusions with Bit6ExclusionsModule;
-	provides IGuiceScanJarExclusions with Bit6ExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.sixbitplatform.implementations.Bit6ExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.sixbitplatform.implementations.Bit6ExclusionsModule;
 
 	opens com.jwebmp.plugins.sixbitplatform to com.fasterxml.jackson.databind, com.jwebmp.core;
 	//opens com.jwebmp.plugins.sixbitplatform.events to com.fasterxml.jackson.databind,com.jwebmp.core;
