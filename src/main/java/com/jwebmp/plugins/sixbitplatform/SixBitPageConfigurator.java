81c7709fd7dc5a1b5fee170f082baba1c2022039
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:19:29 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/sixbitplatform/SixBitPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/sixbitplatform/SixBitPageConfigurator.java
@@ -24,7 +24,7 @@ import com.jwebmp.core.services.IPageConfigurator;
 import com.guicedee.logger.LogFactory;
 import com.jwebmp.plugins.sixbitplatform.features.SixBitSessionConfigureFeature;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.logging.Logger;
 
 /**
